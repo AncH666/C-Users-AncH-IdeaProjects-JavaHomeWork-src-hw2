93086b813a89a008ad3cf29e6169aafa32e14b80
Anna Parkhomenko <anna.parkhomenko@privatbank.ua>
2021-10-17 19:26:28 +0300

    init commit

--- src/hw2/Calculator.java
+++ src/hw2/Calculator.java
@@ -5,35 +5,38 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int operand1;
-        int operand2;
-        char num = 'A';
-        String symbol;
+        int operand1 = 0;
+        int operand2 = 0;
+        String sign = "";
+        //char num = '';
+        //String symbol = "";
         System.out.println("Необходимо внести первое число");
         operand1 = scan.nextInt();
         System.out.println("Необходимо внести второе число");
         operand2 = scan.nextInt();
-        symbol = scan.next();
+        //symbol = scan.next();
         System.out.println("Необходимо внести -; +; *; или /");
-        String sign = scan.next(); // + - * /
+        sign = scan.next(); // + - * /
 
-        switch (num) {
-            case '-':
-                System.out.println((operand1 - operand2));
+        switch (sign) {
+            case "-":
+                System.out.println(operand1 - operand2);
                 break;
-            case '+':
-                System.out.println((operand1 + operand2));
+            case "+":
+                System.out.println(operand1 + operand2);
                 break;
-            case '*':
-                System.out.println((operand1 * operand2));
-            case '/':
-                System.out.println((operand1 / operand2));
-              if (operand2 == 0) {
+            case "*":
+                System.out.println(operand1 * operand2);
+                break;
+            case "/":
+                if (operand2 != 0) {
+                    System.out.println(operand1 / operand2);
+                } else {
                     System.out.println(" На ноль делить нельзя");
-                    break;
                 }
+                break;
             default:
-                System.out.println(" Неверная операция");
+                System.out.println("Неверная операция");
         }
     }
 }
--- src/hw2/Interval.java
+++ src/hw2/Interval.java
@@ -6,23 +6,19 @@ public class Interval {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int x;
-        System.out.println("Необходимо внести число от 1 до 150");
+        System.out.println("Необходимо внести число от 1 до 100");
         x = scan.nextInt();
 
         if (x >= 0 && x <= 14) {
             System.out.println("Диапазон [0 -14]");
-        }
-        if (x >= 15 && x <= 35) {
+        } else if (x >= 15 && x <= 35) {
             System.out.println("Диапазон [15 -35]");
-        }
-        if (x >= 36 && x <= 50) {
+        } else if (x >= 36 && x <= 50) {
             System.out.println("Диапазон [36 -50]");
-        }
-        if (x >= 51 && x <= 100) {
+        } else if (x >= 51 && x <= 100) {
             System.out.println("Диапазон [51 -100]");
-        }
-        if (x < 0 && x > 100) {
-            System.out.println("Ошибка: Необходимо выбрать число из диапазона");
+        } else {
+            System.out.println("Ошибка: Необходимо выбрать число из диапазона. Введено число больше 100 или меньше 0.");
         }
     }
 }
